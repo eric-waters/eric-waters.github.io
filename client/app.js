9c4b5678204f7ec5af2764df8a82ed15fd7a5f54
Eric Waters <eswaters@asu.edu>
2022-02-09 18:37:46 -0700

    fixed email sending

--- client/app.js
+++ client/app.js
@@ -47,7 +47,7 @@ function sendEmail(nameInput, orgInput, emailInput, messageInput) {
             Body : `${nameInput} (${emailInput}) from ${orgInput} has sent the following message:
             ${messageInput}`
         }).then(
-        message => alert('Thanks! I will get back to you as soon as I can.')
+            message => alert('Thanks! I will get back to you as soon as I can.')
         ).catch(message => 
             alert(message)
         );
@@ -62,12 +62,13 @@ document.getElementById('sendEmailButton').addEventListener('click', () => {
     var orgInput = document.getElementById('orgInput');
     var emailInput = document.getElementById('emailInput');
     var messageInput = document.getElementById('messageInput');
-    sendEmail(nameInput.value, orgInput.value, emailInput.value, messageInput.value);
-    //clear input values
-    nameInput.value = '';
-    orgInput.value ='';
-    emailInput.value = '';
-    messageInput.value = '';
+    if(sendEmail(nameInput.value, orgInput.value, emailInput.value, messageInput.value)) {
+        //clear input values
+        nameInput.value = '';
+        orgInput.value ='';
+        emailInput.value = '';
+        messageInput.value = '';
+    }
 });
 
 //Text animation function at top of screen
